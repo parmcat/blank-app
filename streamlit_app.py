d453369afe05da99f945096983f784201ddbd488
parmcat <44008888+parmcat@users.noreply.github.com>
2026-05-14 17:11:45 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,19 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+)
+
+import pandas as pd
+import altair as alt
+df = pd.read_parquet("./hk_hospital_wait.parquet", engine="pyarrow")
+
+alt.data_transformers.enable('json')
+
+(alt.Chart(df)
+ .mark_rect()
+ .encode(x='week:O',
+         y='wkday_name:O' 
+        ,color=alt.Color('mean(max_wait):Q', scale=alt.Scale(scheme='orangered')))
 )
